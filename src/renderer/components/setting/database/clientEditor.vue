0571f0ae6117f5c0bc6d243cd76ff64690ae0f64
UnitedPOS <UnitedPOS.service@gmail.com>
2017-09-30 13:48:38 -0400

    *create file for incoming log system

---
+++ src/renderer/components/setting/database/clientEditor.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="editor">
+        <header>
+            <h2>{{$t('title.customerInfo')}}</h2>
+        </header>
+        <div class="inner">
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    props:['init'],
+    
+}
+</script>
