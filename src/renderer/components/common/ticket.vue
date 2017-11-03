baa4fd3a49ece26fc1e3c7f7fc728d0b4ac17c7d
UnitedPOS <UnitedPOS.service@gmail.com>
2017-11-03 12:37:58 -0400

    *update

--- src/renderer/components/common/ticket.vue
+++ src/renderer/components/common/ticket.vue
@@ -7,40 +7,40 @@
 
 <script>
 export default {
-    props: ['init'],
-    created() {
-        this.ticket = Printer.preview(this.init.ticket);
+  props: ["init"],
+  created() {
+    this.ticket = Printer.preview(this.init.ticket);
+  },
+  data() {
+    return {
+      ticket: null
+    };
+  },
+  methods: {
+    fn() {
+      this.init.exit && this.init.resolve();
     },
-    data() {
-        return {
-            ticket: null
-        }
-    },
-    methods: {
-        fn() {
-            this.init.exit && this.init.resolve()
-        },
-        print() {
-            Printer.setTarget('Receipt').print(this.init.ticket)
-        }
+    print() {
+      Printer.setTarget("Receipt").print(this.init.ticket);
     }
-}
+  }
+};
 </script>
 
 <style scoped>
 .receipt {
-    background: #fff;
-    width: 280px;
-    padding: 35px 20px 5px;
-    box-shadow: 0 1px 8px rgba(0, 0, 0, 0.5);
+  background: #fff;
+  width: 280px;
+  padding: 35px 20px 5px;
+  box-shadow: 0 1px 8px rgba(0, 0, 0, 0.5);
 }
 
 .print {
-    position: fixed;
-    left: 0;
-    right: 0;
-    margin: auto;
-    padding: 15px;
-    bottom: 30px;
+  position: fixed;
+  left: 0;
+  right: 0;
+  margin: auto;
+  padding: 15px;
+  bottom: 30px;
 }
 </style>
