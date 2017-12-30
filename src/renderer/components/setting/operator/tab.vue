c7d4c594053f0a857091065293b713bfe9935e34
huangxuewu <huangxuewu@gmail.com>
2017-12-30 01:27:55 -0500

    *update

--- src/renderer/components/setting/operator/tab.vue
+++ src/renderer/components/setting/operator/tab.vue
@@ -4,7 +4,7 @@
       <router-link tag="li" class="tab" :to="{name:'Setting.operator'}">{{$t('title.operators')}}</router-link>
       <template v-if="operator">
         <router-link tag="li" class="tab" :to="{name:'Setting.operator.config',params:{operator}}">{{$t('setting.basic')}}</router-link>
-        <router-link tag="li" class="tab" :to="{name:'Setting.operator.timecard',params:{operator}}">{{$t('title.timecard')}}</router-link>
+        <router-link tag="li" class="tab" :to="{name:'Setting.operator.timecard',params:{operator}}">{{$t('setting.timecard')}}</router-link>
       </template>
     </ul>
     <transition name="slide" mode="out-in">
