7625baf5d23c0db332bdb197515571fed6d41f75
huangxuewu <huangxuewu@gmail.com>
2018-01-19 00:10:28 -0500

    *adding exadigm parser

--- src/renderer/components/payment/parser/exadigm.js
+++ src/renderer/components/payment/parser/exadigm.js
@@ -1,9 +1,16 @@
+const net = require('net');
+const socket = new net.Socket();
+
 const Exadigm = function () {
-    let url = null;
+    let host = null;
+    let port = null;
     let auth = null;
-    this.initial = function (ip, port, authCode) {
-        url = `http://${ip}:${port}`;
+
+    this.initial = function (host, port, authCode) {
+        host = host;
+        prot = port || 8765;
         auth = authCode;
+
         return new Promise((resolve) => {
             resolve({
                 text() {
@@ -22,6 +29,35 @@ const Exadigm = function () {
     };
     this.explainTransaction = function (data) {
 
+    };
+    this.connect = function () {
+        if (!host || !port) throw Error("Please Initial first");
+
+        socket.connect(host, port, () => {
+
+            socket.write(JSON.stringify(
+                {
+                    "packetID": "XCRP",
+                    "packetData": {
+                        "packetAuthorization": "987pjasd4687",
+                        "requestID": 0,
+                        "baseAmount": 0,
+                        "receiptCount": 0,
+                        "transactionType": 0,
+                        "taxAmount": 0
+                    }
+                }
+            ))
+        });
+
+        socket.on('data', (data) => {
+            console.log(data);
+            socket.destroy();
+        });
+
+        socket.on('close', () => {
+            console.log("connection closed")
+        })
     }
 };
 
