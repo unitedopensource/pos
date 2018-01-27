a45fee18635ea3b6dbfbb6f5e25ac499ee0e1d77
UnitedPOS <UnitedPOS.service@gmail.com>
2018-01-26 19:06:38 -0500

    *update

--- src/renderer/components/setting/sales/revenue.vue
+++ src/renderer/components/setting/sales/revenue.vue
@@ -1,22 +1,73 @@
 <template>
     <div>
         <range-tab @update="fetchData" initial="currentWeek"></range-tab>
+        <div class="table">
+            <table class="data">
+                <thead>
+                    <tr>
+                        <th class="date">{{$t('thead.date')}}</th>
+                        <th>{{$t('thead.weeks')}}</th>
+                        <th>{{$t('type.WALK_IN')}}</th>
+                        <th>{{$t('type.PICK_UP')}}</th>
+                        <th>{{$t('type.DELIVERY')}}</th>
+                        <th>{{$t('type.DINE_IN')}}</th>
+                        <th>{{$t('type.HIBACHI')}}</th>
+                        <th>{{$t('type.BUFFET')}}</th>
+                        <th>{{$t('type.SALES')}}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(ledger,index) in ledgers" :key="index">
+                        <td class="date">{{ledger.date}}</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <!-- <tr>
+                        <td colspan="9" class="f1">
+                            <div class="summary">
+                                <div class="static">
+                                    <p class="date"></p>
+                                    <p class="field">
+                                        <span class="text">{{$t('setting.batchRecord')}}
+                                            <span class="value"></span>
+                                        </span>
+                                        <span class="text">{{$t("text.total")}}
+                                            <span class="value"></span>
+                                        </span>
+                                    </p>
+                                </div>
+                                <div class="option">
+
+                                </div>
+                            </div>
+                        </td>
+                    </tr> -->
+                </tfoot>
+            </table>
+            <div :is="component" :init="componentData" @refresh="fetchData"></div>
+        </div>
     </div>
 </template>
 
 <script>
 import rangeTab from "../common/rangeTab";
 export default {
-    components:{ rangeTab },
-    data() {
-        return {
+  components: { rangeTab },
+  data() {
+    return {
+      componentData: null,
+      component: null,
+      ledgers:[]
+    };
+  },
+  methods: {
+    fetchData(range) {
 
-        }
-    },
-    methods: {
-        fetchData(range) {
-
-        }
     }
-}   
+  }
+};
 </script>
